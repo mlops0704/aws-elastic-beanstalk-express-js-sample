ee1542ed29dedaf4081a3d5c2c10796201523492
mlops <strikermlops@gmail.com>
2024-04-21 11:36:50 +0530

    pipeline is created

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World, good morning!'));
+app.get('/', (req, res) => res.send('Hello World, good morning!. Again happy good morning'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
